b93792b85162e70b96428bf3914c49d470b0c0ef
Rullabcde <choirulrasyid09@gmail.com>
2025-06-22 08:29:45 +0700

    update config mysql

--- master1/init.sql
+++ master1/init.sql
@@ -1,3 +1,3 @@
-CREATE USER 'replication'@'%' IDENTIFIED WITH mysql_native_password BY 'siswa';
+CREATE USER IF NOT EXISTS 'replication'@'%' IDENTIFIED WITH mysql_native_password BY 'siswa';
 GRANT REPLICATION SLAVE ON *.* TO 'replication'@'%';
 FLUSH PRIVILEGES;
